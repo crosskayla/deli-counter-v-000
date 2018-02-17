8b4db11517390f29eab4d0f089c79cf41b9fe01b
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:22:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  return "Thanks #{name} - you are position ! in line"
+  return "Welcome, #{name}. You are number {katz_deli.size} in line"
 end
 
 def now_serving()
