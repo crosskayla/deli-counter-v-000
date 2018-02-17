69bc7aad418e8c0b0553ba77375ab05c73613d37
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:32:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   array.collect!.with_index do |name, index|
     "#{index+1}. #{name} "
   end
-  return "The line is currently: #{array}""
+  return "The line is currently: #{array.to_s}"
 end
 
 def take_a_number(array, name)
