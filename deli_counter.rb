c6919595866b12ac006577c8aaa488c7b68e645d
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:42:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     line_array = array.collect.with_index {|name, index| "#{index+1}. #{name}"}
-    return "The line is currently: " + line_array.join(" ")
+    puts "The line is currently: " + line_array.join(" ")
   end
 end
 
