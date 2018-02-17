b5d72476dd6a8da27caa3fb4ed5caa211f303e3f
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:41:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 katz_deli = []
 
 def line(array)
-  line_array = array.collect.with_index {|name, index| "#{index+1}. #{name}"}
-  return "The line is currently: " + line_array.join(" ")
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    line_array = array.collect.with_index {|name, index| "#{index+1}. #{name}"}
+    return "The line is currently: " + line_array.join(" ")
+  end
 end
 
 def take_a_number(array, name)
