9136219637086232cf21741e5cbc75514071cef6
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:23:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 katz_deli = []
 
-def line(katz_deli)
+def line(array)
+  array.each_with_index do |name|
+    
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
+def take_a_number(array, name)
+  array << name
   return "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
