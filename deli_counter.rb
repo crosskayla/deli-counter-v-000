b10ea313acc8ead619b1765e693b9670623e8e6c
Kayla Cross <kac288@georgetown.edu>
2018-02-17 17:33:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,7 @@ katz_deli = ["Kayla", "Melanie"]
 
 def line(array)
   line_array = array.collect.with_index {|name, index| "#{index+1}. #{name}"}
-  end
-  return "The line is currently: " + array.join(" ")
+  return "The line is currently: " + line_array.join(" ")
 end
 
 def take_a_number(array, name)
